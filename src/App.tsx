782b7f7eb2b56af634598d58ad4365afa4043abd
Norbert Goździk <norbert1303@wp.pl>
2022-05-13 19:46:48 +0200

    Basic structure of main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import {Test} from 'types';
-import './App.css';
 
-function App() {
-  const foobar: Test = {
-    x:23
-  }
+
+export const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+     <header>
+       <h1>
+         <strong>Mega</strong> Ogłoszenia
+       </h1>
+       <button>Dodaj ogłoszenie</button>
+       <div className="seatch">
+         <input type="text"/> <button>Szukaj</button>
+       </div>
+     </header>
+     <div className='map'>
+       ...
+     </div>
+   </>
   );
 }
 
-export default App;
+
